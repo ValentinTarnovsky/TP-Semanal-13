67923380bbb58cac99c08fbfc92d99c1fbaf7482
Valentin Tarnovsky <tarnovsky.valentin@gmail.com>
2024-02-26 09:25:53 -0300

    trim fix

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     };
 
     const handleOnChangeValue = (val) => {
-        taskRef.current = val;
+        taskRef.current = val.trim();
     };
 
     const handleOnClickSubmit = (e) => {
@@ -38,15 +38,15 @@ const App = () => {
 
     const TaskForm = () => {
         return (
-            <div className="taskForm bg-dark p-3 pb-1">
-                <h2 className="taskForm__title text-light">Task Form</h2>
-                <form className="taskForm__form">
+            <div className="bg-dark p-3 pb-1">
+                <h2 className="text-light">Task Form</h2>
+                <form>
                     <div className="d-flex">
-                        <input className="taskForm__form__input form-control me-2" type="text" name="task-form" id="task-form" placeholder="Add task" minLength={40} onChange={(e) => handleOnChangeValue(e.target.value)}/>
-                        <button className="taskForm__form__btn btn btn-light" onClick={handleOnClickSubmit}><FontAwesomeIcon icon={faPlus} /></button>
+                        <input className="form-control me-2" type="text" name="task-form" id="task-form" placeholder="Add task" minLength={40} onChange={(e) => handleOnChangeValue(e.target.value)}/>
+                        <button className="btn btn-light" onClick={handleOnClickSubmit}><FontAwesomeIcon icon={faPlus} /></button>
                     </div>
                     <br />
-                    <span className="err text-danger" id="err"></span>
+                    <span className="text-danger" id="err"></span>
                 </form>
             </div>
         );
@@ -54,19 +54,19 @@ const App = () => {
 
     const TaskList = () => {
         return (
-            <div className="taskList bg-dark p-3 pt-0 mt-3">
+            <div className="bg-dark p-3 pt-0 mt-3">
                 <div className="d-flex justify-content-between">
-                    <h2 className="taskList__title text-light">Task List</h2>
-                    <h3 className="taskList__subtitle text-light">Total Tasks: {tasks.length}</h3>
+                    <h2 className="text-light">Task List</h2>
+                    <h3 className="text-light">Total Tasks: {tasks.length}</h3>
                 </div>
-                <ul className="taskList__list list-group">
+                <ul className="list-group">
                     {tasks.map((task, index) => (
-                        <li className="taskList__item list-group-item bg-dark text-light d-flex justify-content-between align-items-center" key={index}>
+                        <li className="list-group-item bg-dark text-light d-flex justify-content-between align-items-center" key={index}>
                             <div className="d-flex flex-column">
                                 <span className="fw-bold">ID: {task.id}</span>
                                 <span>{task.text}</span>
                             </div>
-                            <button className="taskList__item__btn btn btn-danger" onClick={() => handleOnChangeDelete(index)}><FontAwesomeIcon icon={faTrash} /></button>
+                            <button className="btn btn-danger" onClick={() => handleOnChangeDelete(index)}><FontAwesomeIcon icon={faTrash} /></button>
                         </li>
                     ))}
                 </ul>
